e2e60643c8562032bf9f95d650390b3ea0dbdb66
mr1anderson <65372457+mr1anderson@users.noreply.github.com>
2020-12-09 20:17:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
   ],
   author: {
     name: 'Ms Amel H',
-    photo: '/photo.jpg',
+    photo: '/photo2.jpg',
     bio: 'CEM english blog, you will find all courses plus aditional files here',
       contacts: {
       email: '',
